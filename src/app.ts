5293d357d67a6f814cee336bfdd2397a7495898a
Guilherme Vezula <vezula18@gmail.com>
2025-11-27 09:37:15 -0300

    create user routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import cors from "cors";
 import express from "express";
+import userRoutes from "./routes/user.routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+//Routes
+app.use("/users", userRoutes);
 
 export default app;
